a635ed196d30b7480419f77bc3202819cd33c320
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-05 14:17:06 -0400

    added notes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
-
+////////FUNCTION AS ARGUMENTS///////////
 function repeat(fn,n){
   for(let i = 0; i < n; i++){
-    console.log(fn());
+    fn();
   }
- 
+
 }
 
 function hello(){
@@ -17,6 +17,7 @@ function goodBye(){
 //repeat(goodBye,5);
 //repeat(hello,5);
 
+///////////FUNCTIONS AS ARGUMENTS(2)/////////////
 
 const myNames = ['Rich','Joe','Bhaumk','Ray'];
 //////////////////fn = function(name) => fn(parameter)
@@ -42,16 +43,36 @@ const filteredNames = filter(myNames, function(name) {
   return name[0] === 'R';
   
 });
-console.log(filteredNames); // => ['Rich', 'Ray']
+//console.log(filteredNames); // => ['Rich', 'Ray']
 
 
 
-function hazardWarningCreator(typeOfWarnging){
+////////////FUNCTIONS AS RETURN VALUES//////////
+function hazardWarningCreator(typeOfWarning){
   let warningCounter = 0;
-
+  //const rocksWarning(arguement)
   return function(location){
+
     warningCounter++;
+    let once = 'time';
+    let multi = 'times';
+
+    if( warningCounter === 1){
+      console.log(`DANGER! There is a ${typeOfWarning} alert has triggered ${location}`);
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${once} today!`);
+    } if( warningCounter > 1){
+      console.log(`DANGER! There is a ${typeOfWarning} alert has triggered ${location}`);
+      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${multi} today!`);
+    }
   };
 
-
 }
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const iceWarning = hazardWarningCreator('Black ice on the Road');
+const floodWarnging = hazardWarningCreator('Flooding across Road');
+//rocksWarning('Main St and Pacific Ave');
+//rocksWarning('Centinela Ave and Olympic Blv');
+
+//iceWarning('Hall RD and West');
+//iceWarning('Hall RD and King');
