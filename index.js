8f713e9f58bb97a7759473404f87a8f6efb1eba7
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-10-05 09:43:57 -0400

    create filter function. add notes to have a better understanding of how it works.

--- index.js
+++ index.js
@@ -14,5 +14,28 @@ function hello(){
 function goodBye(){
   console.log('Goodbye world');
 }
-repeat(goodBye,5);
+//repeat(goodBye,5);
 //repeat(hello,5);
+
+
+const myNames = ['Rich','Joe','Bhaumk','Ray'];
+//////////////////fn = function(name) => fn(arguement)
+function filter(arr,fn){
+  let newArray = [];//function(name)
+  arr.filter(el => {
+    if(fn(el)){
+      newArray.push(el);
+    }
+  });
+
+
+  return newArray;
+}
+// eslint-disable-next-line indent
+                                    ////function(parameter)
+const filteredNames = filter(myNames, function(name) {
+
+  return name[0] === 'R';
+  
+});
+console.log(filteredNames); // => ['Rich', 'Ray']
